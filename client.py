61bd4b2b7d93b6d0da3fcabacc5ae29cd8150f0b
pawel998998 <100218272+pawel998998@users.noreply.github.com>
2023-02-07 23:54:03 +0100

    Add files via upload

--- client.py
+++ client.py
@@ -1,22 +1,19 @@
-import socket, threading
+import socket, threading, time, json
 from Pgine import *
 
-client_socket = socket.socket()
+#########################################
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('localhost', 25565))
 
-def multiplayer():
-    while True:
-        data = client_socket.recv(1024).decode()
-        data = json.loads(data)
-        client_socket.send(data).encode()
-        data["player_pos"] = [0, 0]
+#########################################
 
-
-threading.Thread(target=multiplayer).start()
-
-
-
-
+while True:
+    data = client_socket.recv(1024).decode()
+    data = json.loads(data)
+    data["player_pos"] = [1]
+    data = json.dumps(data)
+    client_socket.send(data.encode())
 
 
 
--- server.py
+++ server.py
@@ -1,66 +1,47 @@
 import socket
 import _thread, threading
-import json
+import json, time
+
+#########################################
 
 clients = set()
 clients_lock = threading.Lock()
-
 s = socket.socket()
 s.bind(("localhost", 25565))
-s.listen(1)
+s.listen()
 
-player_data= {
+#########################################
+
+data = {
     "player_id": [],
     "player_pos": [],
     "total_players": 0,
     "player_ip": []
 }
 
-def on_new_client(client,addr):
+#########################################
 
+def on_new_client(client,addr):
+    global data
     with clients_lock:
         clients.add(client)
-        player_data["player_id"].append(addr[1])
-        player_data["player_ip"].append(addr[0])
-        player_data["total_players"] = len(player_data["player_id"])
-        data = json.dumps(player_data)
-        print(player_data)
-    try:    
-        while True:
-            try:
-                recived_data = client.recv(1024).decode()
-            except ConnectionResetError:
-                try:
-                    player_data["player_id"].remove(addr[1])
-                    player_data["player_ip"].remove(addr[0])
-                    player_data["total_players"] -= 1
-                    print(player_data)
-                except:
-                    pass
-            try:
-                if not recived_data:
-                    break
-                else:
-                    with clients_lock:
-                        for c in clients:
-                            client.send(data.encode())
-            except UnboundLocalError:
-                try:    
-                    player_data["player_id"].remove(addr[1])
-                    player_data["player_ip"].remove(addr[0])
-                    player_data["total_players"] -= 1
-                    print(player_data)
+  
+    while True:
+        client.send(json.dumps(data).encode())
+        data = client.recv(1024).decode()
+        data = json.dumps(data)
+        print(data)
+        if not data:
+            break
+        else:
+            with clients_lock:
+                for c in clients:
+                    client.send(json.dumps(data).encode())
 
-                except:
-                    pass
-    finally:
-        with clients_lock:
-            client.close()
+#########################################
 
 print("Server running.")
-
-print(player_data)
-
 while True:
     conn, addr = s.accept()
     _thread.start_new_thread(on_new_client,(conn,addr))
+
